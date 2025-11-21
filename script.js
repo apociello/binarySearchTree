3e21213620da2b1f5b804e9581dd6f349183804e
pociello <asierpociello@gmail.com>
2025-11-21 18:18:35 +0100

    Add Node class and buildTree function with prettyPrint
    - Created Node class for BST nodes
    - Implemented buildTree function to construct a balanced BST from a sorted array
    - Added prettyPrint function to visualize the tree structure
    - Tree class scaffold added but not implemented yet

---
+++ script.js
@@ -0,0 +1,41 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor(array) {
+        this.root = null //
+    }
+}
+
+function buildTree(array, start, end) {
+    if(start > end) return null;
+    const mid = Math.floor((start + end) / 2);
+    const root = new Node(array[mid]);
+
+    root.left = buildTree(array, start, mid - 1);
+    root.right = buildTree(array, mid + 1, end);
+
+    return root;
+}
+
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+
+prettyPrint(buildTree([1, 2, 3, 4, 5, 6,7,9], 0, 7));
